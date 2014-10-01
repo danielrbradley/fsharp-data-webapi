8385b978332d46671798e4916e743bc18cd45ab9
daniel.bradley <daniel.bradley@fundapps.co>
2014-10-01 17:39:50 +0100

    Hack first JsonFormatter together
    - Use minimal test to round-trip model binding.

---
+++ FSharp.Data-WebApi/JsonFormatter.fs
@@ -0,0 +1,27 @@
+﻿namespace FSharp.Data.WebApi
+
+open FSharp.Data.Runtime
+open FSharp.Data
+
+type JsonFormatter() as formatter = 
+    inherit System.Net.Http.Formatting.MediaTypeFormatter()
+    do formatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("application/json"))
+    override x.CanReadType t = typeof<IJsonDocument>.IsAssignableFrom(t)
+    override x.CanWriteType t = typeof<IJsonDocument>.IsAssignableFrom(t)
+
+    override x.ReadFromStreamAsync(t, stream, contentHeaders, formatterContext) = 
+        async { 
+            let jsonValue = JsonValue.Load(stream)
+            return JsonDocument.Create(jsonValue, "") :> obj
+        }
+        |> Async.StartAsTask
+    
+    override x.WriteToStreamAsync(t, value, stream, content, transport) = 
+        async { 
+            let jsonValue = (value :?> IJsonDocument).JsonValue
+            let writer = new System.IO.StreamWriter(stream)
+            jsonValue.WriteTo(writer, JsonSaveOptions.DisableFormatting)
+            writer.Flush()
+            stream.Flush()
+        }
+        |> Async.StartAsTask :> System.Threading.Tasks.Task
---
+++ Tests/Tests.fs
@@ -0,0 +1,77 @@
+﻿module Tests
+
+open FSharp.Data
+open FSharp.Data.WebApi
+open Microsoft.Owin.Testing
+open FsUnit
+open NUnit.Framework
+open Owin
+open System
+open System.Net
+open System.Net.Http
+open System.Web.Http
+open Swensen.Unquote
+
+type TestModel = JsonProvider< """
+    {
+        "field": "value"
+    }""", RootName = "Test">
+
+[<RoutePrefix("api"); Route("test")>]
+type TestController() =
+    inherit ApiController()
+
+    member x.Get() =
+        TestModel.GetSample()
+
+    member x.Put(model : TestModel.Test) =
+        model
+
+let Configuration(app : IAppBuilder) = 
+    let config = 
+        let config = new HttpConfiguration()
+        config.Formatters.Remove config.Formatters.XmlFormatter |> ignore
+        config.Formatters.Insert(0, new JsonFormatter())
+        config.MapHttpAttributeRoutes()
+        config
+    app.UseWebApi config |> ignore
+
+let server = TestServer.Create(Action<IAppBuilder>(Configuration))
+let client = server.HttpClient
+
+let getStatusCodeAndContent (response : HttpResponseMessage) = async { let! content = response.Content.ReadAsStringAsync
+                                                                                          () |> Async.AwaitTask
+                                                                       return response.StatusCode, content }
+let get (url : string) = async { let! response = client.GetAsync(url) |> Async.AwaitTask
+                                 return! response |> getStatusCodeAndContent }
+
+let put (url : string) jsonContent = 
+    async { 
+        use content = new StringContent(jsonContent, Text.Encoding.UTF8, "application/json")
+        let! response = client.PutAsync(url, content) |> Async.AwaitTask
+        return! response |> getStatusCodeAndContent
+    }
+
+let isSuccess status = (int status) >= 200 && (int status) < 300
+
+let contains substring (content : string) =
+    content.Contains(substring)
+
+[<Test>]
+let ``can write JSON document``() =
+    async {
+        let! status, content = get "api/test"
+        test <@ status = HttpStatusCode.OK @>
+        test <@ content = TestModel.GetSample().JsonValue.ToString(JsonSaveOptions.DisableFormatting) @>
+    }
+    |> Async.RunSynchronously
+
+[<Test>]
+let ``can read JSON document``() =
+    async {
+        let document = TestModel.Test("another value")
+        let! status, content = put "api/test" (document.JsonValue.ToString())
+        test <@ status = HttpStatusCode.OK @>
+        test <@ content = document.JsonValue.ToString(JsonSaveOptions.DisableFormatting) @>
+    }
+    |> Async.RunSynchronously
